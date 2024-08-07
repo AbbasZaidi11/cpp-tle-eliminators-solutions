a19a483a613e0188860fafd9759cf06958ab9be0
Abbas Zaidi <105381187+AbbasZaidi11@users.noreply.github.com>
2024-08-07 19:13:40 +0530

    Update game_with_integers.cpp

--- 800/game_with_integers.cpp
+++ 800/game_with_integers.cpp
@@ -1,1 +1,22 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t)
+    {
+        int n;
+        cin >> n;
+        if (n % 3 == 0)
+        {
+            cout << "Second" << endl;
+        }
+        else
+        {
+            cout << "First" << endl;
+        }
+        t--;
+    }
+    return 0;
+}
