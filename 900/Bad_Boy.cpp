a23a108d6ae8c061d5b398ec4cabb9f34a84a984
Abbas Zaidi <105381187+AbbasZaidi11@users.noreply.github.com>
2025-01-08 14:43:40 +0530

    Create Bad_Boy.cpp

---
+++ 900/Bad_Boy.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--){
+        ll n,m,i,j;
+        cin >> n >> m >> i >> j;
+        ll x1,y1,x2,y2;
+        x1=n;
+        y1=1;
+        x2=1;
+        y2=m;
+        
+        cout << x1 << " " << y1 << " " << x2 << " " << y2 <<endl;
+
+
+    }
+    return 0;
+}
